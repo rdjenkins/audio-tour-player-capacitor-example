9ffc5a13233c041d33e42c59e7a370d020358804
Dean Jenkins <richarddeanjenkins@gmail.com>
2026-05-22 22:49:24 +0100

    Adjusted the timing of module loading and checks so this example reflects how we do it on Celtic Quiet Places

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import 'audio-tour-player'
 import { capacitorStorageDelegate, capacitorJsonLoader ,capacitorUrlRewriter} from './capacitor-bridge.js';
 
 
@@ -6,14 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = document.querySelector('audio-tour-player');
   if (player) {
     // timing is everything in capacitor!
-    // Save and remove src attribute if present
+    // Need a 'remove and replace the src' trick 
+    // In Celtic Quiet Places we build the attribute in the HTML dynamically
+    // so the src is not hardwired as in this example in index.html
     let srcValue = null;
     if (player.hasAttribute('src')) {
       srcValue = player.getAttribute('src');
       player.removeAttribute('src');
     }
 
-    customElements.whenDefined('audio-tour-player').then(() => {
+    import('audio-tour-player').then(() => {
       // Inject Capacitor logic before setting src
       console.log('Injecting Capacitor logic into audio-tour-player.');
       player.storage = capacitorStorageDelegate;
